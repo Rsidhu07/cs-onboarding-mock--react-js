81c2bce1ee9bb38611859f3f59906d41fbe213fa
Rsidhu07 <rajsidhuh07@gmail.com>
2022-02-15 00:41:17 +0530

    basic setup

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={css.Header}>
+      Eden
+    </header>
+  )
+};
+
+export default Header;
---
+++ src/containers/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import css from './LandingPage.module.css';
+
+const LandingPage = () => {
+  return (
+    <div className={css.LandingPage}>
+      <Header/>
+    </div>
+  )
+};
+
+export default LandingPage;
